f8ed5befe5eb162adc091d381c66c06124434c7a
ice-hermes <83634377+ice-hermes@users.noreply.github.com>
2025-04-01 22:40:33 +0300

    weird error

--- tlb/models.go
+++ tlb/models.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/ice-hermes/tongo/boc"
+	"github.com/tonkeeper/tongo/boc"
 )
 
 var ErrGramsOverflow = errors.New("grams overflow")
@@ -36,7 +36,7 @@ func (g *Grams) UnmarshalTLB(c *boc.Cell, decoder *Decoder) error {
 		return err
 	}
 	if ln > 8 {
-		ln = 8
+		return ErrGramsOverflow
 	}
 	var amount uint64
 	for i := 0; i < int(ln); i++ {
